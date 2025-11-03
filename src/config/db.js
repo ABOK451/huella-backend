c3406c02d9443c4fb3b6a6899a2aa69463c4730d
ABOK451 <alancito2013america@gmail.com>
2025-11-03 16:22:11 -0600

    mysql

--- src/config/db.js
+++ src/config/db.js
@@ -4,21 +4,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT || 5432,
-        dialect: "postgres",
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false, // necesario en Render si no usas certificado propio
-            },
-        },
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  }
 );
+
+
 
 export const connectDB = async() => {
     try {
